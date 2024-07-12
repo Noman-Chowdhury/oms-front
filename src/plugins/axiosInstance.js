3507d839b638a463c46bcacdc15e7bec3b779d2b
nomanhowdhury <nomanhowdhury.dev@gmail.com>
2024-07-12 16:14:35 +0600

    make api call instance

---
+++ src/plugins/axiosInstance.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const adminAxiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_APP_URL}`,
+    headers: {
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Bearer ${localStorage.getItem('_a_t')}`
+    }
+});
+
+const userAxiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_APP_URL}`,
+    headers: {
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Bearer ${localStorage.getItem('_u_t')}`
+    }
+});
+
+const axiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_APP_URL}`,
+    headers: {
+        'Accept': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*',
+    }
+});
+
+export { adminAxiosInstance, userAxiosInstance, axiosInstance };
